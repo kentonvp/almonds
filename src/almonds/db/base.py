b74325718a77f81eaf9e526b796a4daaefebe06e
kentonvp <kentonvanp@gmail.com>
2024-06-24 09:33:02 -1000

    Add to ORM

--- src/almonds/db/base.py
+++ src/almonds/db/base.py
@@ -1,4 +1,11 @@
+# isort: skip_file
+
 from almonds.db.database import Base, engine
 
-# Import models here
+# Add database models below Base import
+
+from almonds.models.budget import Budget
+from almonds.models.category import Category
+from almonds.models.goal import Goal
+from almonds.models.transaction import Transaction
 from almonds.models.user import User
