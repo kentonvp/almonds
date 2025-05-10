bbf22b733f696747ba93ce6dea068eb15e15dc32
kentonvp <kentonvanp@gmail.com>
2025-05-09 16:26:06 -1000

    feat: request info tracking with prometheus

--- src/almonds/app.py
+++ src/almonds/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 from almonds.api.budget import budget_bp
 from almonds.api.goals import goal_bp
@@ -11,12 +12,16 @@ from almonds.api.transactions import transaction_bp
 from almonds.db.base import Base, engine
 from almonds.templates.filters import format_currency, format_date, format_dollars
 from almonds.utils.logging import logger
+from src.almonds.utils import status_code
 
 
 def create_app():
     Base.metadata.create_all(engine)
 
     app = Flask(__name__)
+
+    metrics = PrometheusMetrics(app)
+    logger.info("Prometheus metrics initializaed")
 
     if os.getenv("ALMONDS_SECRET") is None:
         raise ValueError("ALMONDS_SECRET environment variable not set")
@@ -41,6 +46,16 @@ def create_app():
     @app.errorhandler(Exception)
     def handle_exception(e):
         logger.error(f"An error occurred: {e}", exc_info=True)
-        return {"error": "An internal error occurred"}, 500
+        return {
+            "error": "An internal error occurred"
+        }, status_code.HTTP_500_SERVER_ERROR
+
+    @app.route("/metrics")
+    def exports_metrics():
+        logger.info("Serving custom metrics")
+        response_data, content_type = metrics.generate_metrics(
+            accept_header="application/json"
+        )
+        return response_data, status_code.HTTP_200_OK, {"Content-Type": content_type}
 
     return app
