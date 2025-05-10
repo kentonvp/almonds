ed75182b8acde5fa5d315761a40c35aafcee9062
kentonvp <kentonvanp@gmail.com>
2025-05-09 15:18:26 -1000

    config: persistent logs

---
+++ src/almonds/utils/logging.py
@@ -0,0 +1,8 @@
+import logging.config
+import os
+
+logging.config.fileConfig(
+    os.path.join(os.path.dirname(__file__), "..", "config", "logging.conf"),
+    disable_existing_loggers=False,
+)
+logger = logging.getLogger("almonds_app")
