cdb1348e19a8f37009d15d853f535f8d329b0359
kentonvp <kentonvanp@gmail.com>
2025-05-15 12:27:32 -1000

    ci: missing logging.conf

--- src/almonds/utils/logging.py
+++ src/almonds/utils/logging.py
@@ -2,7 +2,7 @@ import logging.config
 import os
 
 logging.config.fileConfig(
-    os.path.join(os.path.dirname(__file__), "..", "config", "logging.conf"),
+    os.path.join(os.path.dirname(__file__), "..", "logging.conf"),
     disable_existing_loggers=False,
 )
 logger = logging.getLogger("almonds_app")
